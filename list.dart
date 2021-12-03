ae859100944efe13cfef1935994eba73ebfee94a
DaewonSeo <saz0549@naver.com>
2021-12-03 21:33:03 +0900

    dart List basic

---
+++ list.dart
@@ -0,0 +1,17 @@
+void main(){
+  List btsList = [
+    '진',
+    '슈가',
+    '랩몬스터'
+  ];
+  List<String> redVeletList = [
+    '슬기',
+    '아이린',
+    '웬디'
+  ];
+  
+  print(redVeletList[0]);
+//   print(btsList);
+//   print(redVeletList);
+  print(redVeletList.length);
+}
